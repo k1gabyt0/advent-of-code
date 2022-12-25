7fb50b12fa2d7504fb5eaba4569904a5fef78490
Andrei Kigai <avkigai@gmail.com>
2022-12-25 18:51:13 +0300

    Complete day01-part2

--- 2022/day01/calories.go
+++ 2022/day01/calories.go
@@ -3,9 +3,11 @@ package day01
 import (
 	"bufio"
 	"embed"
+	"errors"
 	"fmt"
 	"io"
 	"log"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -21,37 +23,61 @@ func SolvePuzzle() {
 	defer f.Close()
 
 	fmt.Println("--- Day 1: Calorie Counting ---")
-	fmt.Printf("The answer is: %d", CountMaxCalories(bufio.NewReader(f)))
+	maxCals, err := CountMaxCalories(bufio.NewReader(f), 3)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("The answer is: %d", maxCals)
 }
 
-func CountMaxCalories(in io.Reader) int {
+// CountMaxCalories returns sum of top leadersCount calories.
+func CountMaxCalories(in io.Reader, leadersCount uint) (int, error) {
+	if leadersCount == 0 {
+		return 0, errors.New("leaders count can't be 0")
+	}
+	var elfCals []int
+
 	scanner := bufio.NewScanner(in)
 
-	maxCals := 0
 	curCals := 0
 	for scanner.Scan() {
 		calsRecord := strings.TrimSpace(scanner.Text())
 		if calsRecord == "" {
-			if curCals > maxCals {
-				maxCals = curCals
-
+			if curCals != 0 {
+				elfCals = append(elfCals, curCals)
 			}
 			curCals = 0
 			continue
 		}
 
-		curCals += must(strconv.Atoi(calsRecord))
+		calsRecordNumber, err := strconv.Atoi(calsRecord)
+		if err != nil {
+			return 0, err
+		}
+
+		curCals += calsRecordNumber
 	}
 	if scanner.Err() != nil {
-		log.Fatal(scanner.Err())
+		return 0, scanner.Err()
+	}
+	if curCals != 0 {
+		elfCals = append(elfCals, curCals)
 	}
 
-	return maxCals
+	sort.Sort(sort.Reverse(sort.IntSlice(elfCals)))
+	return sumLeaders(elfCals, leadersCount), nil
 }
 
-func must[T any](arg T, err error) T {
-	if err != nil {
-		panic(err)
+func sumLeaders(leaderboard []int, leadersCount uint) int {
+	sum := 0
+	handledLeadersCount := 0
+	for i := 0; i < len(leaderboard); i++ {
+		if handledLeadersCount == int(leadersCount) {
+			break
+		}
+
+		sum += leaderboard[i]
+		handledLeadersCount++
 	}
-	return arg
+	return sum
 }
--- 2022/day01/calories_test.go
+++ 2022/day01/calories_test.go
@@ -10,13 +10,15 @@ import (
 
 func TestSolvePuzzle(t *testing.T) {
 	type args struct {
-		in io.Reader
+		in           io.Reader
+		leadersCount uint
 	}
 
 	tests := []struct {
-		name string
-		args args
-		want int
+		name    string
+		args    args
+		want    int
+		wantErr bool
 	}{
 		{
 			name: "Example from AOC site",
@@ -37,14 +39,75 @@ func TestSolvePuzzle(t *testing.T) {
 
 					10000
 				`),
+				leadersCount: 1,
 			},
 			want: 24000,
+		},
+		{
+			name: "Empty record",
+			args: args{
+				in:           strings.NewReader(""),
+				leadersCount: 1,
+			},
+			want: 0,
+		},
+		{
+			name: "Only one record",
+			args: args{
+				in:           strings.NewReader("1000"),
+				leadersCount: 1,
+			},
+			want: 1000,
+		},
+		{
+			name: "0 leaders count should return error",
+			args: args{
+				in:           strings.NewReader("1000"),
+				leadersCount: 0,
+			},
+			wantErr: true,
+		},
+		{
+			name: "Top 3 elves",
+			args: args{
+				in: strings.NewReader(`
+					1000
+					2000
+					3000
+
+					4000
+
+					5000
+					6000
+
+					7000
+					8000
+					9000
+
+					10000
+				`),
+				leadersCount: 3,
+			},
+			want: 45000,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := day01.CountMaxCalories(tt.args.in)
+			got, err := day01.CountMaxCalories(tt.args.in, tt.args.leadersCount)
+			if tt.wantErr {
+				if err == nil {
+					t.Fatal("expected error, but didn't get it")
+				}
+				if got != 0 {
+					t.Fatalf("expected zero value, but got=%d", got)
+				}
+				return
+			}
+			if err != nil {
+				t.Fatalf("unexpected error=%q", err)
+			}
+
 			if got != tt.want {
 				t.Fatalf("got=%d cal, but wanted=%d cal", got, tt.want)
 			}
